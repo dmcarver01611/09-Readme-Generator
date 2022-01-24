d1b91605f7afc0eec36458fa702e02a4ab3fdd85
Damon Carver <dmcarver0611@gmail.com>
2022-01-24 07:58:29 -0500

    added and pushing files and changes

---
+++ Develop/index.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the project name?",
+            name: "project"
+        },
+        {
+            type: "input",
+            message: "What is your github username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your github repo?",
+            name: "repo"
+        },
+        {
+            type: "input",
+            message: "What is your project about?",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "How do you install the project?",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "What is the use?",
+            name: "usage"
+        },
+        {
+            type: "list",
+            message: "What is the license of your project?",
+            name: "license",
+            choices: ['ISC', 'BSD-3-Clause', 'MIT', 'UNLICENSED'],
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email"
+        },
+    ]);
+  };
+
+const generateReadme = ({ project, username, description, installation, usage, license, email, repo }) =>
+    `# ${project}|
+
+    ## Description of Project:
+    ${description}
+
+    ## Installation:
+    Install node, go to my repo at ${repo} and clone the project from there.
+    ${installation}
+    
+    ## Usage:
+    ${usage}
+
+    ## License:
+    ${license}
+
+    ## Questions:
+    If you have any questions feel free to look at my personal Github Profile ${username} or reach out to me
+    via my email at ${email}. I'll respond as soon as possible!
+    `
+
+const init = () => {
+    questions()
+      .then((answers) => fs.writeFileSync('README2.md', generateReadme (answers)))
+      .then(() => console.log('Successfully wrote to README2.md'))
+      .catch((err) => console.error(err));
+  };
+
+init();
---
+++ README2.md
@@ -0,0 +1,19 @@
+# Readme Generator - By Damon Carver|
+
+    ## Description of Project:
+    A professional readme generator for developers
+
+    ## Installation:
+    Install node, go to my repo at https://github.com/dmcarver01611/09-Readme-Generator and clone the project from there.
+    Run "node index.js' in the proper file location after cloning the project to your computer
+    
+    ## Usage:
+    Saving developers time by making readme files automatically
+
+    ## License:
+    UNLICENSED
+
+    ## Questions:
+    If you have any questions feel free to look at my personal Github Profile dmcarver01611 or reach out to me
+    via my email at dmcarver0611@gmail.com. I'll respond as soon as possible!
+    
